032c5b52620cd0e8ae5ea76161114782f777d95e
Bartek M <bartek87m@gmail.com>
2021-03-08 12:53:34 +0100

    Started to work on bottom navigation bar

--- lib/presentation/training/pages/trainings_page.dart
+++ lib/presentation/training/pages/trainings_page.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:motion_tab_bar/motiontabbar.dart';
 import 'package:training_log/application/auth/auth_cubit.dart';
 import 'package:training_log/application/workoutWatcher/workoutwatcher_bloc.dart';
 import 'package:training_log/presentation/routes/router.gr.dart';
@@ -9,7 +10,12 @@ import 'package:training_log/presentation/training/widgets/workoutsViewsWidget.d
 
 import '../../../injection.dart';
 
-class TrainingsPage extends StatelessWidget {
+class TrainingsPage extends StatefulWidget {
+  @override
+  _TrainingsPageState createState() => _TrainingsPageState();
+}
+
+class _TrainingsPageState extends State<TrainingsPage> {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
@@ -48,6 +54,21 @@ class TrainingsPage extends StatelessWidget {
             children: [
               CreateNewWorkout(),
               WorkoutsViewsWidget(),
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: 0, // this will be set when a new tab is tapped
+            items: [
+              BottomNavigationBarItem(
+                icon: new Icon(Icons.home),
+                title: new Text('Home'),
+              ),
+              BottomNavigationBarItem(
+                icon: new Icon(Icons.mail),
+                title: new Text('Messages'),
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), title: Text('Profile'))
             ],
           ),
         ),
