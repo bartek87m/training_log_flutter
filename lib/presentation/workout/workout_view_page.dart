13de2194c8c00c16755e5ca5e505dbfd6ef61c12
Bartek M <bartek87m@gmail.com>
2021-07-07 07:32:16 +0200

    Working workout_view_page

--- lib/presentation/workout/workout_view_page.dart
+++ lib/presentation/workout/workout_view_page.dart
@@ -20,22 +20,72 @@ class WorkoutViewPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
-            child: TextFormField(
-              initialValue: workout.title!.getOrCrash(),
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
+                child: TextFormField(
+                  initialValue: workout.title!.getOrCrash(),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
+                child: Column(
+                  children: workout.exercieList!
+                      .map(
+                        (exercise) => Container(
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                initialValue:
+                                    exercise.exerciseName!.getOrCrash(),
+                              ),
+                              Row(
+                                children: [
+                                  Column(
+                                    children: exercise.setsList!
+                                        .map(
+                                          (element) => Container(
+                                            margin: EdgeInsets.only(
+                                                left: 1.w, right: 1.w),
+                                            height: 5.h,
+                                            width: 40.w,
+                                            child: TextFormField(
+                                              initialValue: element.reps!,
+                                            ),
+                                          ),
+                                        )
+                                        .toList(),
+                                  ),
+                                  Column(
+                                    children: exercise.setsList!
+                                        .map(
+                                          (element) => Container(
+                                            margin: EdgeInsets.only(
+                                                left: 1.w, right: 1.w),
+                                            height: 5.h,
+                                            width: 40.w,
+                                            child: TextFormField(
+                                              initialValue: element.result!,
+                                            ),
+                                          ),
+                                        )
+                                        .toList(),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ],
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
-            child: TextFormField(
-              initialValue: 'Add wotkout here',
-              maxLines: null,
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
