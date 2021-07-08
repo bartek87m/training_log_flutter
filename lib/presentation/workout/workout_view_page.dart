b3966ebf9774aaec00d064463ffc9f37827f5a6c
Bartek M <bartek87m@gmail.com>
2021-07-08 14:49:37 +0200

    Continue working on workout_page_view

--- lib/presentation/workout/workout_view_page.dart
+++ lib/presentation/workout/workout_view_page.dart
@@ -1,91 +1,131 @@
 import 'package:flutter/material.dart';
+import 'package:training_log/domain/workout/exercise/exercise.dart';
+import 'package:training_log/domain/workout/series/series.dart';
+import 'package:training_log/domain/workout/value_objects.dart';
 import 'package:training_log/domain/workout/workout.dart';
 import 'package:sizer/sizer.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class WorkoutViewPage extends StatelessWidget {
+class WorkoutViewPage extends HookWidget {
   final Workout workout;
 
   const WorkoutViewPage({Key? key, required this.workout}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final workoutExercises = workout.exercieList!
-        .forEach((element) => print(element.exerciseName!.getOrCrash()));
+    final workout = useState(Workout.newWorkout());
+    final toggleRebuild = useState(false);
 
-    // final workoutSets = workout.exercieList!
-    //     .forEach((element) => print(element.exerciseName!.getOrCrash()));
-
-    // final workoutExercises = workout.exercieList!
-    //     .forEach((element) => print(element.exerciseName!.getOrCrash()));
+    useMemoized(
+      () {
+        workout.value = this.workout;
+      },
+    );
 
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
-                child: TextFormField(
-                  initialValue: workout.title!.getOrCrash(),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 2.h, vertical: 1.w),
-                child: Column(
-                  children: workout.exercieList!
-                      .map(
-                        (exercise) => Container(
-                          child: Column(
-                            children: [
-                              TextFormField(
-                                initialValue:
-                                    exercise.exerciseName!.getOrCrash(),
-                              ),
-                              Row(
-                                children: [
-                                  Column(
-                                    children: exercise.setsList!
-                                        .map(
-                                          (element) => Container(
-                                            margin: EdgeInsets.only(
-                                                left: 1.w, right: 1.w),
-                                            height: 5.h,
-                                            width: 40.w,
-                                            child: TextFormField(
-                                              initialValue: element.reps!,
-                                            ),
-                                          ),
-                                        )
-                                        .toList(),
-                                  ),
-                                  Column(
-                                    children: exercise.setsList!
-                                        .map(
-                                          (element) => Container(
-                                            margin: EdgeInsets.only(
-                                                left: 1.w, right: 1.w),
-                                            height: 5.h,
-                                            width: 40.w,
-                                            child: TextFormField(
-                                              initialValue: element.result!,
-                                            ),
-                                          ),
-                                        )
-                                        .toList(),
-                                  ),
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-            ],
-          ),
+      appBar: AppBar(
+        title: TextFormField(
+          initialValue: workout.value.title!.getOrCrash(),
+          onChanged: (_) {}, //TODO add onChange save title
+          style: TextStyle(color: Colors.white),
+          decoration: InputDecoration(border: InputBorder.none),
+          cursorColor: Colors.white,
         ),
+      ),
+      body: Container(
+        margin: EdgeInsets.only(top: 2.h),
+        child: ListView.builder(
+            itemCount: workout.value.exercieList!.length,
+            itemBuilder: (context, int index) {
+              return Column(
+                children: [
+                  SizedBox(
+                    height: 3.h,
+                    width: 90.w,
+                    child: TextFormField(
+                      initialValue: workout.value.title!.getOrCrash(),
+                    ),
+                  ),
+                  Row(
+                    key: UniqueKey(),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(children: [
+                        for (int i = 0;
+                            i <
+                                workout
+                                    .value.exercieList![index].setsList!.length;
+                            i++)
+                          Row(children: [
+                            Text((i + 1).toString()),
+                            Container(
+                              margin: EdgeInsets.only(left: 1.w, right: 1.w),
+                              height: 3.h,
+                              width: 35.w,
+                              child: TextFormField(
+                                key: Key(i.toString()),
+                                textAlign: TextAlign.center,
+                                initialValue: workout.value.exercieList![index]
+                                    .setsList![i].reps,
+                              ),
+                            ),
+                          ]),
+                      ]),
+                      Column(
+                        children: [
+                          for (int i = 0;
+                              i <
+                                  workout.value.exercieList![index].setsList!
+                                      .length;
+                              i++)
+                            Row(
+                              children: [
+                                Container(
+                                  margin:
+                                      EdgeInsets.only(left: 1.w, right: 1.w),
+                                  height: 3.h,
+                                  width: 35.w,
+                                  child: TextFormField(
+                                    key: Key(i.toString()),
+                                    textAlign: TextAlign.center,
+                                    initialValue: workout
+                                        .value
+                                        .exercieList![index]
+                                        .setsList![i]
+                                        .result,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  child: Icon(Icons.delete),
+                                  onTap: () {
+                                    workout.value.exercieList![index].setsList!
+                                        .removeAt(i);
+                                    toggleRebuild.value = !toggleRebuild.value;
+                                    print(workout
+                                        .value.exercieList![index].setsList!);
+                                  },
+                                ),
+                              ],
+                            ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Container(
+                    // height: 4.h,
+                    // width: 80.w,
+                    child: TextButton(
+                      onPressed: () => {
+                        workout.value.exercieList![index].setsList!
+                            .add(Series.newSeries()),
+                        toggleRebuild.value = !toggleRebuild.value,
+                      },
+                      child: Text('Add series'),
+                    ),
+                  ),
+                ],
+              );
+            }),
       ),
     );
   }
